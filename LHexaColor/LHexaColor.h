5f9d7a5e29793e1fb16df3805fd626f7070e3d3b
LAITH <laith@laith.local>
2018-05-11 22:17:35 +0300

    Initial Commit

---
+++ LHexaColor/LHexaColor.h
@@ -0,0 +1,19 @@
+//
+//  LHexaColor.h
+//  LHexaColor
+//
+//  Created by LAITH on 5/11/18.
+//  Copyright © 2018 LAITH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LHexaColor.
+FOUNDATION_EXPORT double LHexaColorVersionNumber;
+
+//! Project version string for LHexaColor.
+FOUNDATION_EXPORT const unsigned char LHexaColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHexaColor/PublicHeader.h>
+
+
